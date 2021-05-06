744be37d4b7f386fe00067681c9a5ac2a4c5775a
mintyjones <gordon@zeedesigns.com.au>
2021-05-06 16:03:40 +1000

    added hero code to doubleapi branch

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function createDog(dog){
     let dogImage = document.createElement("img")
     dogImage.src = dog[0].url
     dogPool.appendChild(dogImage)
-
 }
 
 function printDogs(dogs){
@@ -23,3 +22,66 @@ getData();
 
 const heroButton = document.getElementById("randomPaw")
 heroButton.addEventListener("click", getData)
+
+const urlHero = 'https://superheroapi.com/api.php/10157727801421556/'
+const numberOfHeroes = 2
+const allHeroData = []
+const apiUrls = ['https://superheroapi.com/api.php/10157727801421556/', 'https://api.thedogapi.com/v1/images/search']
+
+const heroPool = document.querySelector("#heroName")
+
+function randomHero() {
+    let heroId = Math.floor(Math.random()*731)
+    return heroId
+}
+
+function getHeroData(){
+    let heroNum = randomHero();
+    fetch(urlHero+heroNum)
+    .then(response => response.json())
+    .then(data => printHeroDetails(data))
+    .catch(e => {console.log(e)})
+    // heroPool.appendChild()
+}
+
+function getAnimalData(){
+    fetch('https://api.thedogapi.com/v1/images/search')
+        .then(response => response.json())
+        .then(data => {console.log(data)})
+        .catch(e => {console.log(e)})
+}
+
+// function storeHeroData(heroData) {
+//     let allHeroData = []
+//     for (i=0;i<numberOfHeroes;i++) {
+//         let singleHero = []
+        
+//     }
+// }
+
+function printHeroDetails(herodata){
+    // console.log(herodata)
+    // console.log(herodata.name)
+    // console.log(`Combat: ${herodata.powerstats.combat}`)
+    // console.log(`Intelligence: ${herodata.powerstats.intelligence}`)
+    // console.log(`Speed: ${herodata.powerstats.speed}`)
+    // console.log(`Strength: ${herodata.powerstats.strength}`)
+    let heroObj = new Object();
+    heroObj.name = herodata.name
+    heroObj.stats = herodata.powerstats
+    console.log(heroObj)
+    allHeroData.push(heroObj)
+    console.log(allHeroData)
+    // let heroName = document.getElementById("heroName")
+    // heroName.innerText = herodata.name
+}
+
+// const loadHeroesButton = document.getElementById("getHeroes")
+
+// loadHeroesButton.addEventListener('click', getHeroData)
+
+// apiUrls.forEach(url => {
+    for (i=0;i<numberOfHeroes;i++) {
+        getHeroData();
+    }
+// });
