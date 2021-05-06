a5a3fb8ba436f1cd1ef006665e5ab243dba6d889
Kfoster14 <kelleyafoster@hotmail.com>
2021-05-07 09:38:58 +1000

    added pet-name module

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 // dogs fetching code
-function getData(){
-    fetch('https://api.thedogapi.com/v1/images/search')
-        .then(response => response.json())
-        .then(data => {createDog(data)})
-        .catch(e => {console.log(e)})
-}
-const dogPool = document.querySelector(".dogPool")
+// function getData(){
+//     fetch('https://api.thedogapi.com/v1/images/search')
+//         .then(response => response.json())
+//         .then(data => {createDog(data)})
+//         .catch(e => {console.log(e)})
+// }
+// const dogPool = document.querySelector(".dogPool")
 
-function createDog(dog){
-    dogPool.innerHTML = ""
-    let dogImage = document.createElement("img")
-    dogImage.src = dog[0].url
-    dogPool.appendChild(dogImage)
+// function createDog(dog){
+//     dogPool.innerHTML = ""
+//     let dogImage = document.createElement("img")
+//     dogImage.src = dog[0].url
+//     dogPool.appendChild(dogImage)
 
-}
+// }
 
-function printDogs(dogs){
-    console.log(dogs)
-}
+// function printDogs(dogs){
+//     console.log(dogs)
+// }
 
-getData();
+// getData();
 
-const heroButton = document.getElementById("randomPaw")
-heroButton.addEventListener("click", getData)
+// const heroButton = document.getElementById("randomPaw")
+// heroButton.addEventListener("click", getData)
+
+console.log("hi first")
+// const petNames = require('pet-names');
+<script src="your-js" type="module">
+import petNames from 'pet-names';
+const petGroup = petNames.random();
+console.log('hi')
+console.log(petGroup)
+let petName = document.getElementById("petname");
+petName.innerText = petGroup
